7425f4ba8c3552d36954bf95ee3e2efff17f1334
biiwide <tcushman@gmail.com>
2015-05-16 22:26:52 -0500

    Adding hiccup-style and data-style node constructors

--- src/marksism/core.clj
+++ src/marksism/core.clj
@@ -1,6 +1,31 @@
 (ns marksism.core
   (:require [marksism.pegdown :as peg]))
 
-(defn parse [to-node md-string]
-  "Parses a String of Markdown content"
-  (peg/parse to-node md-string))
+(defn parse
+  "Parses a String of Markdown content.
+  cons-node: Node Constructor (fn [node-type node-attrs node-children-or-string])
+  markdown: Markdown content as a String
+
+Two ready to use node constructors are provided: hiccup-sylte & data-style"
+  [cons-node markdown]
+  (peg/parse cons-node markdown))
+
+(defn hiccup-style
+  "A node constructor that returns Hiccup-like data:
+  [:node-type (attrs-map)? child ...]"
+  [type attrs content]
+  (let [content-coll (if (coll? content)
+                       content
+                       (list content))]
+    (cons type
+      (if (empty? attrs)
+        content-coll
+        (cons attrs content-coll)))))
+
+(defn data-style
+  "A node constructor that returns a map in the style of clojure.data.xml and Enlive:
+  {:tag type, :attrs attributes, :content content}"
+  [type attrs content]
+  (cond-> {:tag type :content content}
+    (not-empty attrs)
+    (assoc :attrs attrs)))
